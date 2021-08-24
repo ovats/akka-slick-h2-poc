3ceb7333b2aac7418e5d5fb1495dd8a6696bb9f9
Gustavo Muniz <cgmds73@gmail.com>
2021-08-24 18:11:54 -0300

    Add returning an Option in find CRUD operations

--- common/src/main/scala/com/cc/db/dao/ProductDao.scala
+++ common/src/main/scala/com/cc/db/dao/ProductDao.scala
@@ -10,13 +10,13 @@ import scala.concurrent.Future
 object ProductDao extends BaseDao {
 
   // Standard CRUD
-  def findAll: Future[Seq[Product]]               = productsTable.result
-  def create(product: Product): Future[ProductId] = productsTable.returning(productsTable.map(_.id)) += product
-  def findById(id: ProductId): Future[Product]    = productsTable.filter(_.id === id).result.head
-  def delete(idProd: ProductId): Future[Int]      = productsTable.filter(_.id === idProd).delete
+  def findAll: Future[Seq[Product]]                    = productsTable.result
+  def create(product: Product): Future[ProductId]      = productsTable.returning(productsTable.map(_.id)) += product
+  def findById(id: ProductId): Future[Option[Product]] = productsTable.filter(_.id === id).result.headOption
+  def delete(idProd: ProductId): Future[Int]           = productsTable.filter(_.id === idProd).delete
 
   // Other operations
-  def findByUUID(id: UUID): Future[Product]                  = productsTable.filter(_.uuid === id).result.head
+  def findByUUID(id: UUID): Future[Option[Product]]          = productsTable.filter(_.uuid === id).result.headOption
   def findByVendor(vendorName: String): Future[Seq[Product]] = productsTable.filter(_.vendor === vendorName).result
 
   // For creating products table
