76467b533f7490466a41f1b99ba6249a19719fec
Gustavo Muniz <60721226+ovats@users.noreply.github.com>
2021-08-23 23:00:52 -0300

    Fix little bug in import (#5)

--- api/src/main/scala/com/cc/CCApiMain.scala
+++ api/src/main/scala/com/cc/CCApiMain.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import .pruebasDeMetodosDAO
 import com.cc.config.ApiAppConfig
 import com.cc.db.dao.ProductDao
 import com.cc.routes.{PingRoutes, ProductRoutes}
