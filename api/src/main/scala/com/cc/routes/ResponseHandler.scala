e12a90e69f44ff7c37e7c042d9e0300c3e72d5b7
Gustavo Muniz <cgmds73@gmail.com>
2021-08-31 19:59:39 -0300

    Refactor in response handler

--- api/src/main/scala/com/cc/routes/ResponseHandler.scala
+++ api/src/main/scala/com/cc/routes/ResponseHandler.scala
@@ -12,25 +12,27 @@ import io.circe.generic.auto._
 trait ResponseHandler extends LazyLogging with FailFastCirceSupport {
   def handleServiceResponse(response: ServiceResponse, entity: String): StandardRoute = {
     response match {
+      case ValidationErrors(errors) =>
+        val errorMessage = s"Validation errors ($entity): ${errors.toString}"
+        logger.debug(errorMessage)
+        complete(StatusCodes.BadRequest, ErrorsResponse(errors))
       case UnknownError(errorMsg) =>
-        val errorMessage = s"There was an error when creating ($entity): $errorMsg"
-        logger.error(errorMessage)
-        complete(StatusCodes.InternalServerError, errorMessage)
+        val errorMessage = s"Error ($entity)"
+        logger.error(s"$errorMessage: $errorMsg")
+        complete(StatusCodes.InternalServerError, ErrorsResponse(List(errorMessage)))
       case r =>
-        val msg = s"Unexpected response ($entity): ${r.toString}"
-        logger.error(msg)
-        complete(StatusCodes.InternalServerError, ApiResponse(status = "error", message = msg))
+        val errorMessage = s"Unexpected response ($entity): ${r.toString}"
+        logger.error(errorMessage)
+        complete(StatusCodes.InternalServerError, ErrorsResponse(List(errorMessage)))
 
     }
   }
 
   def handleFailure(e: Throwable, entity: String): StandardRoute = {
-    val errorMessage = s"Exception has been thrown ($entity)"
+    val errorMessage = s"Error when processing request ($entity)"
     logger.error(errorMessage, e)
-    complete(StatusCodes.InternalServerError, ApiResponse(status = "error", message = errorMessage))
+    complete(StatusCodes.InternalServerError, ErrorsResponse(List(errorMessage)))
   }
 }
 
-//TODO remove ApiResponse
-final case class ApiResponse(status: String = "ok", message: String)
 final case class ErrorsResponse(errors: Seq[String])
--- common/src/main/scala/com/cc/db/dao/ProductDao.scala
+++ common/src/main/scala/com/cc/db/dao/ProductDao.scala
@@ -10,13 +10,13 @@ import scala.concurrent.Future
 object ProductDao extends BaseDao {
 
   // Standard CRUD
-  def findAll: Future[Seq[Product]]                    = productsTable.result
   def create(product: Product): Future[ProductId]      = productsTable.returning(productsTable.map(_.id)) += product
   def findById(id: ProductId): Future[Option[Product]] = productsTable.filter(_.id === id).result.headOption
+  def findByUUID(id: UUID): Future[Option[Product]]    = productsTable.filter(_.uuid === id).result.headOption
   def delete(idProd: ProductId): Future[Int]           = productsTable.filter(_.id === idProd).delete
+  def findAll: Future[Seq[Product]]                    = productsTable.result
 
   // Other operations
-  def findByUUID(id: UUID): Future[Option[Product]] = productsTable.filter(_.uuid === id).result.headOption
   def findByVendor(vendorName: String, caseSensitive: Boolean): Future[Seq[Product]] = {
     if (caseSensitive)
       productsTable.filter(_.vendor === vendorName).result
